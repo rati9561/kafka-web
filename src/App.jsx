a5d23aa254bde36a0451efe5d537102caede3faa
rati9561 <97848209+rati9561@users.noreply.github.com>
2024-12-21 23:42:47 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <nav className=''>
+            <ul>
+              <li>Home</li>
+              <li>Producer</li>
+              <li>Consumer</li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
